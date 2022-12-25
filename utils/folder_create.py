7bee3e8b88b4ce9ebb10501e429d6d994ef86ccd
Mai <21039366+Mai0313@users.noreply.github.com>
2022-12-26 02:45:07 +0800

    add requirements.txt

---
+++ utils/folder_create.py
@@ -0,0 +1,9 @@
+import os
+
+def create_folder():
+    if not os.path.exists('log'):
+        os.makedirs('log')
+    if not os.path.exists('playlist'):
+        os.makedirs('playlist')
+    if not os.path.exists('processed_song'):
+        os.makedirs('processed_song')
